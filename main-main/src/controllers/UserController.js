a0fcb68d082828ca1a55506bc9b1bc2e4812470c
n3vasca <gustavo886688@gmail.com>
2024-10-31 18:42:42 -0300

    first commit

--- main-main/src/controllers/UserController.js
+++ main-main/src/controllers/UserController.js
@@ -26,7 +26,7 @@ const getUserById = async (req, res) => {
 const createUser = async (req, res) => {
   try {
     const newUser = await User.create({ ...req.body }); 
-    res.status(201).json({ message: 'Usuário criado com sucessoo!', newUser });
+    res.status(201).json({ message: 'Usuário criado com sucessooo!', newUser });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
